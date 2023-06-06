d6abde7268d60ba8663f53e0710195c7f25b08ba
ganck <chengkuan@gmail.com>
2023-06-06 16:53:31 +0800

    Fixed camel netty implementation

--- 3ScalePolicyExtensionCamel/README.md
+++ 3ScalePolicyExtensionCamel/README.md
@@ -8,22 +8,6 @@
     oc new-project threescale-camel
 
     ```
-2. Create `keystore.jks` from Ingress Controller:
-
-    ```
-    oc get secret -n openshift-ingress  ingress-certs-2023-06-05 -o go-template='{{index .data "tls.key"}}' | base64 -d | tee tls.key  > /dev/null
-    oc get secret -n openshift-ingress  ingress-certs-2023-06-05 -o go-template='{{index .data "tls.crt"}}' | base64 -d | tee tls.crt  > /dev/null
-    
-    openssl pkcs12 -export -in tls.crt -inkey tls.key -out tls.p12
-
-    keytool -importkeystore -srckeystore tls.p12 \
-        -srcstoretype PKCS12 \
-        -destkeystore keystore.jks \
-        -deststoretype JKS
-    
-    oc create secret generic tls --from-literal=keystore_password=changeit --from-file=keystore.jks=keystore.jks --from-literal=truststore_password=changeit --from-file=truststore.jks=keystore.jks
-
-    ```
 
 2. Go to project folder and run the following command to deploy:
     
@@ -35,7 +19,27 @@
     -Dquarkus.openshift.namespace=threescale-camel \
     -Dquarkus.log.category.\"blog.braindose\".level=DEBUG \
     -Dquarkus.container-image.registry=image-registry.openshift-image-registry.svc:5000 \
-    -Dquarkus.openshift.mounts.tls.path=/tls \
-    -Dquarkus.openshift.secret-volumes.tls.secret-name=tls
+    -Dquarkus.openshift.route.target-port=http \
+    -Dquarkus.openshift.ports.https.container-port=8080 \
+    -Dquarkus.openshift.ports.https.host-port=8080
+
+    ```
+
+## Not Using
+
+1. Create `keystore.jks` from Ingress Controller:
+
+    ```
+    oc get secret -n openshift-ingress  ingress-certs-2023-06-04 -o go-template='{{index .data "tls.key"}}' | base64 -d | tee tls.key  > /dev/null
+    oc get secret -n openshift-ingress  ingress-certs-2023-06-04 -o go-template='{{index .data "tls.crt"}}' | base64 -d | tee tls.crt  > /dev/null
     
+    openssl pkcs12 -export -in tls.crt -inkey tls.key -out tls.p12
+
+    keytool -importkeystore -srckeystore tls.p12 \
+        -srcstoretype PKCS12 \
+        -destkeystore keystore.jks \
+        -deststoretype JKS
+    
+    oc create secret generic tls --from-literal=keystore_password=changeit --from-file=keystore.jks=keystore.jks --from-literal=truststore_password=changeit --from-file=truststore.jks=keystore.jks -n threescale-camel
+
     ```
--- 3ScalePolicyExtensionCamel/src/main/java/blog/braindose/demo/threescale/Routes.java
+++ 3ScalePolicyExtensionCamel/src/main/java/blog/braindose/demo/threescale/Routes.java
@@ -1,69 +1,131 @@
 package blog.braindose.demo.threescale;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Locale;
 import java.util.Random;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.netty.http.NettyHttpMessage;
 import org.apache.camel.model.RouteDefinition;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import blog.braindose.demo.threescale.model.Loan;
 import blog.braindose.demo.threescale.service.JsonMapper;
-
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import jakarta.ws.rs.InternalServerErrorException;
 
 public class Routes extends RouteBuilder {
 
     private static Logger LOGGER = LoggerFactory.getLogger(Routes.class);
 
-    JsonMapper mapper = new JsonMapper();
+    private JsonMapper mapper = new JsonMapper();
     // @ConfigProperty(name = "quarkus.http.port")
     // String port;
 
     @Override
     public void configure() throws Exception {
-        
+
         final RouteDefinition from;
 
-        // LOGGER.info("http port = {}", port);
-
-        // port = (port == null || port.equals("")) ? "8080" : port;
-
         if (Files.exists(keystorePath())) {
-            from = from("netty-http:proxy://0.0.0.0:8443?ssl=true&keyStoreFile=/tls/keystore.jks&passphrase=changeit&trustStoreFile=/tls/truststore.jks");
+            from = from(
+                    "netty-http:proxy://0.0.0.0:8443?ssl=true&keyStoreFile=/tls/keystore.jks&passphrase=changeit&trustStoreFile=/tls/truststore.jks");
         } else {
             from = from("netty-http:proxy://0.0.0.0:8080");
         }
 
         from
-            .log("Message received : ${body}")
-            .process(Routes::enrich)
-            .toD("netty-http:"
-                + "${headers." + Exchange.HTTP_SCHEME + "}://" 
-                + "${headers." + Exchange.HTTP_HOST + "}:"
-                + "${headers." + Exchange.HTTP_PORT + "}"
-                + "${headers." + Exchange.HTTP_PATH + "}")
-            ;
-        
+                .log("Message received : ${body}")
+                .process(this::enrich)
+                .log("Enriched Body Message : ${body}")
+                //.setHeader("CamelHttpMethod").simple("${headers." + Exchange.HTTP_METHOD + "}")
+                //.setHeader("Content-Type").simple("${headers." + Exchange.CONTENT_TYPE + "}")
+                .toD("netty-http:"
+                        + "${headers." + Exchange.HTTP_SCHEME + "}://"
+                        + "${headers." + Exchange.HTTP_HOST + "}:"
+                        + "${headers." + Exchange.HTTP_PORT + "}"
+                        + "${headers." + Exchange.HTTP_PATH + "}");
+
     }
 
     Path keystorePath() {
         return Path.of("/tls", "keystore.jks");
     }
 
-    public static void enrich(final Exchange exchange) { 
+    public void enrich(final Exchange exchange) throws StreamReadException, DatabindException, IOException {
+
+        final NettyHttpMessage message = exchange.getIn(NettyHttpMessage.class);
+        final FullHttpRequest request = message.getHttpRequest();
+        final ByteBuf buf = request.content();
+        final String string = buf.toString(StandardCharsets.UTF_8);
+
+        LOGGER.debug("string = {}", string);
+
+        ObjectMapper m = new ObjectMapper();
         Random ran = new Random();
-        final Message message = exchange.getIn();
-        Loan loan = message.getBody(Loan.class);
-        loan.setCustomerName(loan.getFirstName() + "" + loan.getLastName());
-        loan.setFirstName(null);
-        loan.setLastName(null);
-        loan.setCreditRating(ran.nextDouble(0.1, 1.00));
-        message.setBody(loan);
+        // Loan l1 = message.getBody(Loan.class); // ---- this also return null
+        Loan loan = (Loan) m.readValue(string.getBytes(), Loan.class);
+
+        if (loan != null) {
+            LOGGER.debug("loan: {}", mapper.str(loan));
+            loan.setCustomerName(loan.getFirstName() + " " + loan.getLastName());
+            loan.setFirstName(null);
+            loan.setLastName(null);
+            loan.setCreditRating(ran.nextDouble(0.1, 1.00));
+            LOGGER.debug("", mapper.str(loan));
+        } else {
+            LOGGER.debug("loan object is null");
+            throw new InternalServerErrorException("The post body is empty or null. Expecting Loan details to be posted.");
+        }
+
+        // buf.resetWriterIndex();
+        // ByteBufUtil.writeUtf8(buf, mapper.str(loan));
+        message.setBody(mapper.str(loan).getBytes());   // need to set back into the body. 
     }
+
+    /*
+     * message.getBody() will not work and return null value
+     * public void enrich(final Exchange exchange) {
+     * Random ran = new Random();
+     * final Message message = exchange.getIn();
+     * byte[] body2 = (byte[]) message.getBody();
+     * String body = null;
+     * 
+     * if (body2 != null){
+     * body = new String(body2);
+     * }
+     * 
+     * LOGGER.debug("body: {}", body);
+     * Loan loan = message.getBody(Loan.class);
+     * if (loan != null){
+     * LOGGER.debug("loan: {}", mapper.str(loan));
+     * loan.setCustomerName(loan.getFirstName() + "" + loan.getLastName());
+     * loan.setFirstName(null);
+     * loan.setLastName(null);
+     * loan.setCreditRating(ran.nextDouble(0.1, 1.00));
+     * message.setBody(loan);
+     * }
+     * else{
+     * LOGGER.debug("loan object is null.");
+     * }
+     * 
+     * 
+     * }
+     */
 
 }
